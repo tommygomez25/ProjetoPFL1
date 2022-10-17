6f061b14506da0a73e1152622cf253149fd20f21
tommygomez25 <maysan54321@gmail.com>
2022-10-17 10:39:53 +0100

    fix on filter monomials with exponent zero

--- AddPoly.hs
+++ AddPoly.hs
@@ -9,13 +9,16 @@ addMono a b = if (vars (head a) == vars (head b)) then [Mono (coef (head a) + co
 addMonoKnowingTheyCanBeAdded :: Mono -> Mono -> Mono
 addMonoKnowingTheyCanBeAdded a b = Mono (coef a + coef b) (vars a)
 
-isExponentZero :: Mono -> Mono -> Bool
-isExponentZero a b = foldl (&&) True list
-                        where list = [snd x == 0 | x <- vars a] ++ [snd y == 0 | y <- vars b]
+cleanVarsWithExpZero:: [(Char,Int)] -> [(Char,Int)]
+cleanVarsWithExpZero l | length auxList == 0 = [('-',0)]
+                       | otherwise = auxList
+                          where auxList = [x | x<-l, snd x /= 0]
+
+filterExpZero :: Poly -> Poly
+filterExpZero p = [Mono (coef x) (cleanVarsWithExpZero (vars x)) | x <- p]
+
 -- Function to add two polynomials, the parameter is the two polynomials concatenated and ordered by variable and degree
 addPoly :: Poly -> Poly
 addPoly [] = []
 addPoly [x] = [x]
-addPoly (x:y:xs) | isExponentZero x y = addPoly ((Mono (coef x + coef y) ([('-',0)])) : xs)
-                 | vars x == vars y = addPoly ((addMonoKnowingTheyCanBeAdded x y):xs)
-                 | otherwise = (x: (addPoly (y:xs)))
+addPoly (x:y:xs) = if(vars x == vars y) then (addPoly ((addMonoKnowingTheyCanBeAdded x y):xs) ) else (x : (addPoly (y:xs)))
